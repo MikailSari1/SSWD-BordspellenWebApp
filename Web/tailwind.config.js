ee8d9559005acd2f63d7ec6d6e8bc92004ff052e
MikailSari1 <m.sari1@student.avans.nl>
2024-10-15 11:18:51 +0200

    color themes added for tailwind

---
+++ Web/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './Pages//*.cshtml',
+    './Views//*.cshtml'
+  ],
+  theme: {
+    extend: {
+        colors: {
+            'primary': '#3490dc',
+            'secondary': '#ffed4a',
+            'font-color': '#e3342f',
+        },
+    },
+  },
+  plugins: [],
+}
