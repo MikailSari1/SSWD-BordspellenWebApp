f357cad875f5c1063a75dd2827ad9298dfcaa3d3
MikailSari1 <m.sari1@student.avans.nl>
2024-10-21 21:41:07 +0200

    controller for said file

---
+++ Web/Controllers/OrganizeBoardGameNightController.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+namespace Web.Controllers;
+
+public class OrganizeBoardGameNightController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
