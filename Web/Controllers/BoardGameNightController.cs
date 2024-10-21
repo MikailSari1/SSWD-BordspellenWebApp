1a7cde9bc363547b0dc598c9ed53cf8dc2528899
MikailSari1 <m.sari1@student.avans.nl>
2024-10-21 21:39:41 +0200

    added controllers for said pages

---
+++ Web/Controllers/BoardGameNightController.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+namespace Web.Controllers;
+
+public class BoardGameNightController : Controller
+{
+    public IActionResult Index()
+    {
+        /*var  = new boardGameNight("Game Night", new DateTime(2022, 1, 1, 18, 0, 0), new Address("1234 Main St", "Springfield", 1234));*/
+        return View();
+    }
+}
