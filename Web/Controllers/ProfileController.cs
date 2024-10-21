0f10033ae61bb13926cde0b3a7fb73b9827f8a1e
MikailSari1 <m.sari1@student.avans.nl>
2024-10-21 21:40:50 +0200

    controller for said file

---
+++ Web/Controllers/ProfileController.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+namespace Web.Controllers;
+
+public class ProfileController : Controller
+{
+    public IActionResult Index()
+    {
+        var user = new User("John Doe", "jd12@gmail.com", Gender.Male, new Address("1234 Main St", "Springfield", 1234));
+        return View(user);
+    }
+}
